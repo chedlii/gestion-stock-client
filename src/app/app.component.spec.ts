e8a34e59e12ce36cb01050228944ef0a68f06f31
chedli <chedlicris66@yahoo.fr>
2019-09-19 15:59:09 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gestion-stock-client app is running!');
+    expect(compiled.querySelector('span').textContent).toContain('gestion-stock-client app is running!');
   });
 });
